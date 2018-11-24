7ab89f79396f148d00a4894570d594577df200ed
ksh1283f <ksh1283ff@gmail.com>
2018-11-24 23:34:53 +0900

    2 test

--- New Unity Project/Assets/TestScript.cs
+++ New Unity Project/Assets/TestScript.cs
@@ -1,15 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System;
 public class TestScript : MonoBehaviour
 {
-
+    List<int> numList = new List<int>();
 	
 	void Start ()
     {
-        Debug.Log("Start");		
-	}
+        Debug.Log("Start");
+        Debug.Log("Stat2");
+
+        int i = 0;
+        int j = 1;
+        int k = 3;
+
+        numList.Add(i);
+        numList.Add(j);
+        numList.Add(k);
+    }
 	
 	
 }
